52ad7003555527f60d5e06f3ae46011ca2027dab
arizusen@gmail.com <arizusen@gmail.com@79fbc774-650c-7af1-a47f-15a53eab926f>
2013-05-30 11:02:46 +0000

    Minimo cambio en añadir noticia para que funcione.

--- trunk/ComplunewsJava/src/is/UsuarioReg.java
+++ trunk/ComplunewsJava/src/is/UsuarioReg.java
@@ -4,57 +4,59 @@ import java.util.ArrayList;
 
 import FactoryDao.UsuarioRegDao;
 
-public class UsuarioReg extends Usuario implements UsuarioRegDao
-{
+public class UsuarioReg extends Usuario implements UsuarioRegDao {
 
 	private ArrayList<Noticia> favoritos;
-	
 	
 	public UsuarioReg(String idUsuario){
 		super(idUsuario);
 		this.setFavoritos(new ArrayList<Noticia>());
 	}
-	public String getIdUsuario()
-	{
-		return super.getIdUsuario();
-	}
+	
 	public boolean puntuarComentario(int puntuacion) {
 		
 		return false;
 	}
+	
 	@Override
 	public boolean cambiarContrasena(String newPass) {
 		
 		return false;
 	}
+	
 	@Override
 	public boolean anadirComentario(String newComentario) {
 		
 		return false;
 	}
+	
 	@Override
 	public boolean valorarNoticia(int puntuacion) {
 		
 		return false;
 	}
+	
 	@Override
 	public boolean anadirNoticiaAFavoritos(Noticia noticia) {
 		
-		if(this.favoritos.contains(noticia)){
-		this.favoritos.add(noticia);
-		return true;
+		if(!this.favoritos.contains(noticia)){
+			this.favoritos.add(noticia);
+			return true;
 		}
 		else
-		return false;
+			return false;
 	}
+	
 	@Override
 	public boolean cambiarDatos(Datos newDatos) {
 		
 		return false;
 	}
+	
 	public ArrayList<Noticia> getFavoritos() {
 		return favoritos;
 	}
+	
 	public void setFavoritos(ArrayList<Noticia> favoritos) {
 		this.favoritos = favoritos;
 	}
