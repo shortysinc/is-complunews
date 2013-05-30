7579f8de29a9ff89a1a08b0876f09281e778132a
arizusen@gmail.com <arizusen@gmail.com@79fbc774-650c-7af1-a47f-15a53eab926f>
2013-05-30 00:05:53 +0000

    Terminados los tests de usuario

--- trunk/ComplunewsJava/testValidadorPrueba/is/testValidador/UsuarioTest.java
+++ trunk/ComplunewsJava/testValidadorPrueba/is/testValidador/UsuarioTest.java
@@ -8,67 +8,35 @@ import is.*;
 
 public class UsuarioTest {
 
-
+	Usuario usuarioTest;
+	private String idUsuario;
+	private String contrasena;
+	
+	
 	@Before
 	public void setUp() throws Exception {
-		/*placeName = "place name";
-		placeDescription = "place description";
-		placeTest = new Place(placeName, false, placeDescription);   */
+		idUsuario = "id usuario";
+		contrasena = "contrasena";
+		usuarioTest = new Usuario(idUsuario);
+
 	}
 
 	@Test
-	public void testIsSpaceship() {
-		/*assertFalse("ERROR: We have created a place that does not represent a spaceship but isSpaceship returns true",placeTest.isSpaceship());
-		// Change the place
-		placeTest = new Place("", true, "");
-		assertTrue("ERROR: We have created a place that represents a spaceship but isSpaceship returns false",placeTest.isSpaceship());*/
+	public void testGetPass() {
+		usuarioTest.setPass(contrasena);
+		assertTrue("ERROR: La contraseña devuelta no coincide",usuarioTest.getPass().equals(contrasena));
 	}
 
 	@Test
-	public void testToString() {
-		/*assertTrue("ERROR: the place name does not appear in the string", placeTest.toString().contains(placeName));
-		assertTrue("ERROR: the place description does not appear in the string", placeTest.toString().contains(placeDescription));*/
+	public void testGetIdUsuario() {
+		assertTrue("ERROR: El nombre de usuario no coincide", usuarioTest.getIdUsuario().contains(idUsuario));
 	}
 
 	@Test
-	public void testAddItem() {
-		/*assertTrue("ERROR: The place is empty but addItem returns false", placeTest.addItem(new MockItem()));
-		assertFalse("ERROR: The place contains an item with the same id but addItem returns true", placeTest.addItem(new MockItem()));
-		assertTrue("ERROR: The place does not contain an item with the same id but addItem returns false", placeTest.addItem(new MockItem(MockItem.WRONG_NAME)));*/
-	}
-
-	@Test
-	public void testPickItem() {
-		/*assertNull("ERROR: The place is empty but pickItem returns an object", placeTest.pickItem(MockItem.DEF_NAME));
-		Item testItem = new MockItem();
-		if (placeTest.addItem(testItem)) {
-			Item actualItem;
-			assertNull("ERROR: The place does not contain an item with this id but pickItem returns an object", placeTest.pickItem(MockItem.WRONG_NAME));
-			assertNotNull("ERROR: The place contains an item with this id but pickItem returns null", actualItem=placeTest.pickItem(MockItem.DEF_NAME));
-			assertEquals("ERROR: The place contains an item with this id but pickItem returns null", testItem, actualItem);			
+	public void testSetIdUsuario() {
+		String nuevaID = "nuevaID";
+		usuarioTest.setIdUsuario(nuevaID);
+		assertTrue("ERROR: La ID recibida no se corresponde con la ID nueva", usuarioTest.getIdUsuario().contains(nuevaID));
 		}
-		else {
-			fail("ERROR: addItem is not working properly. Try first addItem tests");
-		}*/
-	}
 	
-	@Test
-	public void testExistItem() {
-		/*assertFalse("ERROR: The place is empty but existItem returns true", placeTest.existItem(MockItem.DEF_NAME));
-		Item testItem = new MockItem();
-		if (placeTest.addItem(testItem)) {
-			assertFalse("ERROR: The place does not contain an item with this id but existItem returns true", placeTest.existItem(MockItem.WRONG_NAME));
-			assertTrue("ERROR: The place contains an item with this id but existItem returns false", placeTest.existItem(MockItem.DEF_NAME));		
-		}
-		else {
-			fail("ERROR: addItem is not working properly. Try first addItem tests");
-		}*/
-	}
-	
-	@Test
-	public void testDropItem() {
-		/*assertTrue("ERROR: The place is empty but dropItem returns false", placeTest.dropItem(new MockItem()));
-		assertFalse("ERROR: The place contains an item with the same id but dropItem returns true", placeTest.dropItem(new MockItem()));
-		assertTrue("ERROR: The place does not contain an item with the same id but dropItem returns false", placeTest.dropItem(new MockItem(MockItem.WRONG_NAME)));*/
-	}
 }
